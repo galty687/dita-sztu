f40351c21afdd307f1316f6d3cb15b788352ed6f
Zhijun Gao <galty687@gmail.com>
2024-11-23 10:22:32 +0800

    add dita source and docs to the repo

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+var keywords=[{w:"\u4F7F\u7528HDMI\u7EBF\u8FDB\u884C\u6295\u5C4F",p:["p0"]},{w:"\u5982\u4F55\u4F7F\u7528VGA\u6295\u5C4F",p:["p1"]},{w:"\u4F7F\u7528VGA\u5C06iPad\u4E0E\u6295\u5F71\u4EEA\u76F8\u8FDE",p:["p2"]},{w:"Context",p:["p3"]},{w:"Sensitive",p:["p3"]},{w:"Help",p:["p3"]},{w:"Airplay\u6295\u5C4F\u6B65\u9AA4",p:["p4"]},{w:"\u5982\u4F55\u4F7F\u7528HDMI\u6295\u5C4F",p:["p5"]},{w:"\u5982\u4F55\u4F7F\u7528Airplay\u5C06iPad\u6295\u5C4F\u81F3\u6295\u5F71\u4EEA",p:["p6"]},{w:"AirPlay\u53C2\u6570\u4FE1\u606F",p:["p7"]},{w:"\u4E0D\u80FD\u4F7F\u7528AirPlay\u6295\u5C4F",p:["p8"]},{w:"VGA\u63A5\u53E3",p:["p9"]},{w:"Airplay",p:["p10"]},{w:"\u4EC0\u4E48\u662FHDMI",p:["p11"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p11"]=[13];
+ph["p2"]=[2];
+ph["p3"]=[3, 4, 5];
+ph["p4"]=[6];
+ph["p5"]=[7];
+ph["p6"]=[8];
+ph["p7"]=[9];
+ph["p8"]=[10];
+ph["p9"]=[11];
+ph["p10"]=[12];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {6:4,11:5,8:4,10:4,0:5,4:-1,9:1,2:1,7:4,5:-1,1:-1};
